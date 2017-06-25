584063e4d01704966b7bd7f5ebe3ecd7e5fd1c7e
Sureddi, Manoj (ESI) <manoj.wolverine8@gmail.com>
2017-06-26 01:40:44 +0530

    final design

---
+++ js/constants.js
@@ -0,0 +1,34 @@
+const constants = {
+    animation: {
+        speed: 800,
+        easing: 'easeInOutExpo'
+    },
+    doughNutchartOptions: {
+        color: '#aaa',
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 4,
+        trailWidth: 1,
+        easing: 'easeInOut',
+        duration: 1400,
+        text: {
+            autoStyleContainer: true
+        },
+        from: { color: '#aaa', width: 1 },
+        to: { color: '#999', width: 4 },
+        // Set default step function for all animate calls
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+            circle.path.setAttribute('stroke-width', state.width);
+
+            var value = Math.round(circle.value() * 100);
+            if (value === 0) {
+                circle.setText('');
+            } else {
+                circle.setText(value+"%");
+            }
+
+        }
+    },
+    divs:["home","about","skills","experience"]
+}
